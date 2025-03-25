5c8ab7f60413313ba05030821cd5a290f805d3db
apostoliseq <apostolisn@hotmail.com>
2025-03-25 16:32:03 +0000

    Add Windows PC to authorized_keys of cluster too

--- ssh-copy-id.sh
+++ ssh-copy-id.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for ip in 192.168.225.146 192.168.225.147 192.168.225.148 192.168.225.149 192.168.225.150; do
-    for key in id_ed25519.pub ~/.ssh/ansible.pub; do
+    for key in ~/.ssh/id_ed25519.pub ~/.ssh/ansible.pub ~/.ssh/apost.pub; do
         ssh-copy-id -i $key $ip
     done
 done
