19824140e4f0d7e3fb644086ef7d72a03545cb3a
apostoliseq <apostolisn@hotmail.com>
2025-03-31 16:04:43 +0000

    Install kubectl and nginx in controllers

--- ssh-copy-id.sh
+++ ssh-copy-id.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Keys: Workstation, ansible, Windows
-for ip in 192.168.225.146 192.168.225.147 192.168.225.148 192.168.225.149 192.168.225.150; do
+for ip in kube-controller-1 kube-controller-2 kube-worker-1 kube-worker-2 kube-loadbalancer; do
     for key in ~/.ssh/id_ed25519.pub ~/.ssh/ansible.pub ~/.ssh/apost.pub; do
         ssh-copy-id -i $key $ip
     done
